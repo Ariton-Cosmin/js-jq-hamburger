e4dd1ecdd749f74eaa888ab7a30bdafea0fe94e1
Cosmin-Ariton <cosminiris12@gmail.com>
2021-04-20 16:35:21 +0200

    js-first

---
+++ main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.header-right > a').click(function() {
+        $('.hamburger-menu').addClass('active');
+    });
+
+    $('.close').click(function() {
+        $('.hamburger-menu').removeClass('active');
+    });
+});
